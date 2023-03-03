6643df742bcc7a7ce34b3327a8429721554bb8e2
Makarand Hinge <mmh2212002@gmail.com>
2023-03-03 14:36:58 +0530

    First start with Java

---
+++ Addition.java
@@ -0,0 +1,10 @@
+class Addition
+{
+public static void main(String args[])
+{
+ int a=2,b=3,c;
+ c=a+b;
+ System.out.println(c);
+
+}
+}
---
+++ GreaterSmaller.java
@@ -0,0 +1,15 @@
+class GreaterSmaller
+{
+public static void main(String args[])
+{
+int a=2,b=4;
+if(a>b)
+{
+System.out.println("a is greater");
+}
+else
+{
+System.out.println("b is greater");
+}
+}
+}
