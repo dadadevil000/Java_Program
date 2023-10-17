431299691f102052867593f526549c61cb2f1753
Harsh Bailurkar <113308692+Harshbailurkar@users.noreply.github.com>
2023-10-17 11:00:41 +0530

    Add files via upload

---
+++ classAndObj.java
@@ -0,0 +1,16 @@
+class student{
+     
+    int rollno;
+    String studentName="harsh";
+    }
+
+public class classAndObj{
+
+   public static void main(String[] args) {
+    
+
+       student obj1= new student();
+       System.out.println(obj1.rollno=42);
+        System.out.println(obj1.studentName);
+   }
+}
